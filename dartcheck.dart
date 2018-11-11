cdb20364513403bf6734f75df1aa33cd49b70777
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 17:09:36 -0600

    update editorconfig

--- dartcheck.dart
+++ dartcheck.dart
@@ -1,82 +1,82 @@
 #library("dartcheck");
 
 genBool() {
-	return Math.random() > 0.5;
+    return Math.random() > 0.5;
 }
 
 genInt(max) {
-	var i = (Math.random() * max).toInt();
+    var i = (Math.random() * max).toInt();
 
-	if (genBool()) {
-		return i * -1;
-	}
-	else {
-		return i;
-	}
+    if (genBool()) {
+        return i * -1;
+    }
+    else {
+        return i;
+    }
 }
 
 genDouble(max) {
-	var d = Math.random() * max;
+    var d = Math.random() * max;
 
-	if (genBool()) {
-		return d * -1;
-	}
-	else {
-		return d;
-	}
+    if (genBool()) {
+        return d * -1;
+    }
+    else {
+        return d;
+    }
 }
 
 genByte() {
-	return genInt(256).abs();
+    return genInt(256).abs();
 }
 
 genList(gen) {
-	var len = genInt(100).abs();
+    var len = genInt(100).abs();
 
-	var lst = [];
+    var lst = [];
 
-	for (var i = 0; i < len; i++) {
-		lst.add(gen());
-	}
+    for (var i = 0; i < len; i++) {
+        lst.add(gen());
+    }
 
-	return lst;
+    return lst;
 }
 
 genCharCode() {
-	return genInt(128).abs();
+    return genInt(128).abs();
 }
 
 genString() {
-	return new String.fromCharCodes(genList(genCharCode));
+    return new String.fromCharCodes(genList(genCharCode));
 }
 
 forAll(property, generators) {
-	var passedAllTests = true;
+    var passedAllTests = true;
 
-	for (var i = 0; i < 100; i++) {
-		var testCase = [];
+    for (var i = 0; i < 100; i++) {
+        var testCase = [];
 
-		for (var j = 0; j < generators.length; j++) {
-			testCase.add(generators[j]());
-		}
+        for (var j = 0; j < generators.length; j++) {
+            testCase.add(generators[j]());
+        }
 
-		print("Test case: ${testCase}");
+        print("Test case: ${testCase}");
 
-		// Awaiting (apply) or (curry) implementation in Dart.
-		// http://www.dartlang.org/articles/emulating-functions/
-		var result = property.call.apply(testCase);
+        // Awaiting (apply) or (curry) implementation in Dart.
+        // http://www.dartlang.org/articles/emulating-functions/
+        var result = property.call.apply(testCase);
 
-		if (!result) {
-			passedAllTests = false;
+        if (!result) {
+            passedAllTests = false;
 
-			print("*** Failed!");
-			print("${testCase}");
+            print("*** Failed!");
+            print("${testCase}");
 
-			break;
-		}
-	}
+            break;
+        }
+    }
 
-	if (passedAllTests) {
-		print("+++ OK, passed 100 tests");
-	}
+    if (passedAllTests) {
+        print("+++ OK, passed 100 tests");
+    }
 }
--- example.dart
+++ example.dart
@@ -3,25 +3,25 @@
 #import("dartcheck.dart");
 
 propEven(i) {
-	return i % 2 == 0;
+    return i % 2 == 0;
 }
 
 genSmall() {
-	return genInt(1000);
+    return genInt(1000);
 }
 
 genEven() {
-	var i = genSmall(1000);
+    var i = genSmall(1000);
 
-	if (i % 2 != 0) {
-		return i + 1;
-	}
-	else {
-		return i;
-	}
+    if (i % 2 != 0) {
+        return i + 1;
+    }
+    else {
+        return i;
+    }
 }
 
 main() {
-	forAll(propEven, [genSmall]);
-	forAll(propEven, [genEven]);
+    forAll(propEven, [genSmall]);
+    forAll(propEven, [genEven]);
 }
